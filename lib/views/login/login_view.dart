c965e2d3e27fe5aa1b8c4f6c8e866fbcf927737d
walczakrepcak <kacperwalczakjarocin@gmail.com>
2026-01-06 22:34:47 +0100

    zmiana StatelessWidget na StatefullWidget oraz zmiana nazwy przycisku

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -6,39 +6,46 @@ import 'package:flutter/material.dart';
 
 import '../register/register_view.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                SizedBox(height: 62),
-                Image.asset(AppImages.logo),
-                SizedBox(height: 21),
-                _signInText,
-                SizedBox(height: 46),
-                BasicTextFormField(initialValue: 'email',),
-                SizedBox(height: 40),
-                BasicTextFormField(initialValue: '12345'),
-                ElevatedButton(
-                  child: const Text('Open route'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute<void>(
-                        builder: (context) => const RegisterView(),
-                      ),
-                    );
-                  },
-                ),
-              ],
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  SizedBox(height: 62),
+                  Image.asset(AppImages.logo),
+                  SizedBox(height: 21),
+                  _signInText,
+                  SizedBox(height: 46),
+                  BasicTextFormField(initialValue: 'email',),
+                  SizedBox(height: 40),
+                  BasicTextFormField(initialValue: '12345'),
+                  ElevatedButton(
+                    child: const Text('Sign in'),
+                    onPressed: () { // dodać walidację
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (context) => const RegisterView(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
