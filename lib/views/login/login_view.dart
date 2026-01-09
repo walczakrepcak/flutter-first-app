8f695a7e5e16947699bdc59600a7f059f69f38dc
walczakrepcak <kacperwalczakjarocin@gmail.com>
2026-01-09 21:25:33 +0100

    walidacja zaszytych danych przy logowaniu

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -41,13 +41,14 @@ class _LoginViewState extends State<LoginView> {
                     TextFormField(
                       controller: _emailController,
                       decoration: const InputDecoration(hintText: 'Email'),
+                      // Walidacja danych
                       validator: (value) {
                         if (value == null ||
                             value.isEmpty ||
                             !value.contains('@')) {
                           return 'Invalid email';
                         }
-                        return null;
+                        return null; // Brak błędu
                       },
                     ),
 
@@ -60,16 +61,30 @@ class _LoginViewState extends State<LoginView> {
                       decoration: const InputDecoration(hintText: 'Password'),
                     ),
 
+                    // Przycisk logowania i jego funkcje
                     ElevatedButton(
                       child: const Text('Sign in'),
                       onPressed: () {
+                        // Wywolanie walidacji formularza za pomoca klucza
                         if (_formKey.currentState!.validate()) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const RegisterView(),
-                            ),
-                          );
+                          // Sprawdzenie czy dane są zgodne z tymi przypisanymi
+                          if (_emailController.text == 'test@dsw.pl' &&
+                              _passwordController.text == '12345') {
+                            // Jeżeli tak, to przejscie do Home (tymczasowo RegisterView)
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterView(),
+                              ),
+                            );
+                          } else {
+                            // Jeżeli nie, to wyswietlenie komunikatu na dole
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Błędny email lub hasło!'),
+                              ),
+                            );
+                          }
                         }
                       },
                     ),
